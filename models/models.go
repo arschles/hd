5822d5baf40b22b6cecf6e9f7c96b041b44ed815
Aaron Schlesinger <arschles@gmail.com>
2018-07-23 21:04:19 -0500

    logs

--- models/models.go
+++ models/models.go
@@ -14,6 +14,7 @@ var DB *pop.Connection
 func init() {
 	var err error
 	env := envy.Get("GO_ENV", "development")
+	log.Println("connecting to DB environment", env)
 	DB, err = pop.Connect(env)
 	if err != nil {
 		log.Fatal(err)
